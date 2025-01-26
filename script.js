a5b82c9fbd97fb0b5dbbae8dee5a2ce12acac1ac
RIFKYGANTENG11 <150894207+RIFKYGANTENG11@users.noreply.github.com>
2025-01-26 22:42:13 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Example of a simple script for smooth scrolling
+document.querySelectorAll('nav a, .sidebar a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
